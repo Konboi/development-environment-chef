0ef887563355c5f09ea9a2fd509858ae74209d76
Konboi <ryosuke.yabuki@gmail.com>
2013-12-15 17:46:31 +0900

    Created workspace and project directory

---
+++ site-cookbooks/workspace/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: workspace
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+directory "/home/vagrant/workspace" do
+  owner "vagrant"
+  group "vagrant"
+  mode 0755
+  action :create
+  not_if { File.exists? "/home/vagrant/workspace" }
+end
