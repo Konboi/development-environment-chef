844d921b1f309b71d1ec69ddd0af3c61bce47f48
Konboi <ryosuke.yabuki@gmail.com>
2014-01-01 17:11:29 +0900

    Added directory

---
+++ site-cookbooks/workspace/recipes/git.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: workspace
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+directory "/home/vagrant/git" do
+  owner "vagrant"
+  group "vagrant"
+  mode 0755
+  action :create
+  not_if { File.exists? "/home/vagrant/git" }
+end
+
